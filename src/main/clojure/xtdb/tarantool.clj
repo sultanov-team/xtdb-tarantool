616ae68f2e010bd9832f4dbc563f501dba2ba5cb
Ilshat Sultanov <just.sultanov@gmail.com>
2021-12-01 01:57:04 +0300

    Add defaults mappers and builders

--- src/main/clojure/xtdb/tarantool.clj
+++ src/main/clojure/xtdb/tarantool.clj
@@ -1,6 +1,49 @@
-(ns xtdb.tarantool)
+(ns xtdb.tarantool
+  (:require
+    [clojure.string :as str])
+  (:import
+    (io.tarantool.driver.api.retry
+      TarantoolRequestRetryPolicies$AttemptsBoundRetryPolicyFactory$Builder)
+    (io.tarantool.driver.mappers
+      DefaultMessagePackMapperFactory)
+    (java.util.function
+      Function
+      UnaryOperator)))
 
 
-(defn square
-  [x]
-  (* x x))
+(set! *warn-on-reflection* true)
+
+
+;;
+;; Default mappers and builders
+;;
+
+(def ^{:doc "Modification-safe instance of the messagepack mapper.
+  The instance contains converters for simple types and complex types java.util.Map and java.util.List."}
+  default-complex-types-mapper
+  (.defaultComplexTypesMapper (DefaultMessagePackMapperFactory/getInstance)))
+
+
+(def ^{:doc "Modification-safe instance of the messagepack mapper.
+  The instance already contains converters for simple types."}
+  default-simple-type-mapper
+  (.defaultSimpleTypeMapper (DefaultMessagePackMapperFactory/getInstance)))
+
+
+(defn default-exception-handler
+  "Returns a default exception handler."
+  []
+  (reify
+    Function
+    (apply [_ e]
+      (str/includes? (.getMessage ^Exception e) "Unsuccessful attempt"))))
+
+
+(defn default-request-retry-policy
+  "Returns a default request retry policy."
+  [{:keys [^long delay]
+    :or   {delay 300}}]
+  (reify
+    UnaryOperator
+    (apply [_ policy]
+      (.withDelay ^TarantoolRequestRetryPolicies$AttemptsBoundRetryPolicyFactory$Builder policy delay))))
--- src/test/clojure/xtdb/tarantool_test.clj
+++ src/test/clojure/xtdb/tarantool_test.clj
@@ -1,9 +1,18 @@
 (ns xtdb.tarantool-test
   (:require
     [clojure.test :refer [deftest testing is]]
-    [xtdb.tarantool :as sut]))
+    [xtdb.tarantool :as sut])
+  (:import
+    (java.util.function
+      Function
+      UnaryOperator)))
 
 
-(deftest square-test
-  (testing "dummy test"
-    (is (= 4 (sut/square 2)))))
+(deftest default-exception-handler-test
+  (testing "should be return an instance of java.util.function.Function"
+    (is (instance? Function (sut/default-exception-handler)))))
+
+
+(deftest default-request-retry-policy-test
+  (testing "should be return an instance of java.util.function.UnaryOperator"
+    (is (instance? UnaryOperator (sut/default-request-retry-policy {:delay 300})))))
