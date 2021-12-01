ae28542a2e39a1ddad9409f2314eb72fe0a7a633
Ilshat Sultanov <just.sultanov@gmail.com>
2021-12-01 04:23:05 +0300

    Add an integration test for the tarantool client

--- src/test/clojure/xtdb/tarantool_test.clj
+++ src/test/clojure/xtdb/tarantool_test.clj
@@ -4,6 +4,11 @@
     [xtdb.system :as system]
     [xtdb.tarantool :as sut])
   (:import
+    (io.tarantool.driver.core
+      RetryingTarantoolTupleClient)
+    (java.util.concurrent
+      CompletableFuture
+      ExecutionException)
     (java.util.function
       Function
       UnaryOperator)))
@@ -16,11 +21,26 @@
 (defn prep-module
   [overrides]
   (try
-    (-> {:tarantool-client (merge {:xtdb/module 'xtdb.tarantool/->tnt-client} overrides)}
+    (-> {:tnt-client (merge {:xtdb/module 'xtdb.tarantool/->tnt-client} overrides)}
         (system/prep-system)
-        :tarantool-client)
+        :tnt-client)
     (catch xtdb.IllegalArgumentException e
       (ex-message (ex-cause e)))))
+
+
+(defn start-module
+  [overrides]
+  (-> {:tnt-client (merge {:xtdb/module 'xtdb.tarantool/->tnt-client} overrides)}
+      (system/prep-system)
+      (system/start-system)
+      :tnt-client))
+
+
+(defn get-box-info
+  [tnt-client]
+  (-> ^CompletableFuture (.call tnt-client "box.info" [] sut/default-complex-types-mapper)
+      (.get)
+      (first)))
 
 
 (defn includes?
@@ -36,19 +56,19 @@
 ;; Tests
 ;;
 
-(deftest default-exception-handler-test
-  (testing "should be return an instance of `java.util.function.Function`"
+(deftest ^:unit default-exception-handler-test
+  (testing "should be returned an instance of `java.util.function.Function`"
     (is (instance? Function (sut/default-exception-handler)))))
 
 
 
-(deftest default-request-retry-policy-test
-  (testing "should be return an instance of `java.util.function.UnaryOperator`"
+(deftest ^:unit default-request-retry-policy-test
+  (testing "should be returned an instance of `java.util.function.UnaryOperator`"
     (is (instance? UnaryOperator (sut/default-request-retry-policy {:delay 300})))))
 
 
 
-(deftest ->tnt-client-test
+(deftest ^:unit ->tnt-client-specs-test
   (testing "should be failed by specs"
 
     (is (includes? #"Arg :username required"
@@ -109,3 +129,24 @@
                             :exception-handler    (sut/default-exception-handler)
                             :request-retry-policy (sut/default-request-retry-policy {:delay 500})
                             :retries              3})))))
+
+
+
+(deftest ^:integration ->tnt-client-test
+  (testing "should be throw an exception with a bad connection options"
+    (let [tnt-client (start-module {:host     "localhost"
+                                    :port     3302
+                                    :username "root"
+                                    :password "root"})]
+      (is (instance? RetryingTarantoolTupleClient tnt-client))
+      (is (thrown-with-msg? ExecutionException #"The client is not connected to Tarantool server"
+            (get-box-info tnt-client)))))
+
+
+  (testing "should be returned a successful response from tarantool"
+    (let [tnt-client (start-module {:username "root"
+                                    :password "root"})
+          box-info   (get-box-info tnt-client)]
+      (is (instance? RetryingTarantoolTupleClient tnt-client))
+      (is (= "0.0.0.0:3301" (get box-info "listen")))
+      (is (includes? #"2.8.2" (get box-info "version"))))))
