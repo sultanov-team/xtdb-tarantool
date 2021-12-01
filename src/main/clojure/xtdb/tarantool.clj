4e364f2b7c9d3eabd6e3c2d1a4c6990b0489d052
Ilshat Sultanov <just.sultanov@gmail.com>
2021-12-01 03:22:07 +0300

    Add tarantool client builder

--- src/main/clojure/xtdb/tarantool.clj
+++ src/main/clojure/xtdb/tarantool.clj
@@ -1,7 +1,12 @@
 (ns xtdb.tarantool
   (:require
-    [clojure.string :as str])
+    [clojure.spec.alpha :as s]
+    [clojure.string :as str]
+    [xtdb.system :as system])
   (:import
+    (io.tarantool.driver.api
+      TarantoolClient
+      TarantoolClientFactory)
     (io.tarantool.driver.api.retry
       TarantoolRequestRetryPolicies$AttemptsBoundRetryPolicyFactory$Builder)
     (io.tarantool.driver.mappers
@@ -47,3 +52,55 @@
     UnaryOperator
     (apply [_ policy]
       (.withDelay ^TarantoolRequestRetryPolicies$AttemptsBoundRetryPolicyFactory$Builder policy delay))))
+
+
+
+;; Specs
+
+(s/def ::host ::system/string)
+(s/def ::port ::system/pos-int)
+(s/def ::username ::system/string)
+(s/def ::password ::system/string)
+(s/def ::retries ::system/pos-int)
+(s/def ::exception-handler #(instance? Function %))
+(s/def ::request-retry-policy #(instance? UnaryOperator %))
+
+
+;; Tarantool client
+
+(defn ^TarantoolClient ->tnt-client
+  "Returns a tarantool client as a component of the xtdb.system."
+  {::system/args {:host                 {:doc       "Host"
+                                         :spec      ::host
+                                         :required? true
+                                         :default   "localhost"}
+                  :port                 {:doc       "Port"
+                                         :spec      ::port
+                                         :default   3301
+                                         :required? true}
+                  :username             {:doc       "Username"
+                                         :spec      ::username
+                                         :required? true}
+                  :password             {:doc       "Password"
+                                         :spec      ::password
+                                         :required? true}
+                  :retries              {:doc       "Retries"
+                                         :spec      ::retries
+                                         :default   3
+                                         :required? true}
+                  :exception-handler    {:doc       "Exception handler"
+                                         :spec      ::exception-handler
+                                         :default   (default-exception-handler)
+                                         :required? true}
+                  :request-retry-policy {:doc       "Request retry policy"
+                                         :spec      ::request-retry-policy
+                                         :default   (default-request-retry-policy {:delay 300})
+                                         :required? true}}}
+  [{:keys [^String host ^long port ^String username ^String password ^long retries ^Function exception-handler ^UnaryOperator request-retry-policy]}]
+  (let [tnt-client (-> (TarantoolClientFactory/createClient)
+                       (.withAddress host port)
+                       (.withCredentials username password)
+                       (.build))]
+    (-> (TarantoolClientFactory/configureClient tnt-client)
+        (.withRetryingByNumberOfAttempts retries exception-handler request-retry-policy)
+        (.build))))
--- src/test/clojure/xtdb/tarantool_test.clj
+++ src/test/clojure/xtdb/tarantool_test.clj
@@ -1,6 +1,7 @@
 (ns xtdb.tarantool-test
   (:require
     [clojure.test :refer [deftest testing is]]
+    [xtdb.system :as system]
     [xtdb.tarantool :as sut])
   (:import
     (java.util.function
@@ -8,11 +9,103 @@
       UnaryOperator)))
 
 
+;;
+;; Helper functions
+;;
+
+(defn prep-module
+  [overrides]
+  (try
+    (-> {:tarantool-client (merge {:xtdb/module 'xtdb.tarantool/->tnt-client} overrides)}
+        (system/prep-system)
+        :tarantool-client)
+    (catch xtdb.IllegalArgumentException e
+      (ex-message (ex-cause e)))))
+
+
+(defn includes?
+  [pattern x]
+  (try
+    (boolean (re-find pattern x))
+    (catch Exception _
+      false)))
+
+
+
+;;
+;; Tests
+;;
+
 (deftest default-exception-handler-test
-  (testing "should be return an instance of java.util.function.Function"
+  (testing "should be return an instance of `java.util.function.Function`"
     (is (instance? Function (sut/default-exception-handler)))))
 
 
+
 (deftest default-request-retry-policy-test
-  (testing "should be return an instance of java.util.function.UnaryOperator"
+  (testing "should be return an instance of `java.util.function.UnaryOperator`"
     (is (instance? UnaryOperator (sut/default-request-retry-policy {:delay 300})))))
+
+
+
+(deftest ->tnt-client-test
+  (testing "should be failed by specs"
+
+    (is (includes? #"Arg :username required"
+                   (prep-module {})))
+
+    (is (includes? #"Arg :username = 123[\s\S]+:xtdb\.tarantool\/username"
+                   (prep-module {:username 123})))
+
+    (is (includes? #"Arg :password required"
+                   (prep-module {:username "root"})))
+
+    (is (includes? #"Arg :password = 123[\s\S]+:xtdb\.tarantool\/password"
+                   (prep-module {:username "root"
+                                 :password 123})))
+
+    (is (includes? #"Arg :host = 123[\s\S]+:xtdb\.tarantool\/host"
+                   (prep-module {:username "root"
+                                 :password "root"
+                                 :host     123})))
+
+    (is (includes? #"Arg :port = -123[\s\S]+:xtdb\.tarantool\/port"
+                   (prep-module {:username "root"
+                                 :password "root"
+                                 :host     "127.0.0.1"
+                                 :port     -123})))
+
+    (is (includes? #"Arg :exception-handler = 123[\s\S]+:xtdb\.tarantool\/exception-handler"
+                   (prep-module {:username          "root"
+                                 :password          "root"
+                                 :exception-handler 123})))
+
+    (is (includes? #"Arg :request-retry-policy = 123[\s\S]+:xtdb\.tarantool\/request-retry-policy"
+                   (prep-module {:username             "root"
+                                 :password             "root"
+                                 :exception-handler    (sut/default-exception-handler)
+                                 :request-retry-policy 123})))
+
+    (is (includes? #"Arg :retries = -123[\s\S]+:xtdb\.tarantool\/retries"
+                   (prep-module {:username             "root"
+                                 :password             "root"
+                                 :exception            (sut/default-exception-handler)
+                                 :request-retry-policy (sut/default-request-retry-policy {:delay 500})
+                                 :retries              -123}))))
+
+
+  (testing "should be passed by specs"
+    (is (nil? (prep-module {:username "root" :password "root"})))
+
+    (is (nil? (prep-module {:username "root" :password "root" :exception-handler (sut/default-exception-handler)})))
+
+    (is (nil? (prep-module {:username             "root"
+                            :password             "root"
+                            :exception-handler    (sut/default-exception-handler)
+                            :request-retry-policy (sut/default-request-retry-policy {:delay 500})})))
+
+    (is (nil? (prep-module {:username             "root"
+                            :password             "root"
+                            :exception-handler    (sut/default-exception-handler)
+                            :request-retry-policy (sut/default-request-retry-policy {:delay 500})
+                            :retries              3})))))
