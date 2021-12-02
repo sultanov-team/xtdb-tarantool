16c45d65a18a61e708f267e079a1ec0a28d90297
Ilshat Sultanov <just.sultanov@gmail.com>
2021-12-02 04:41:55 +0300

    Add api initializer

--- src/main/tarantool/server.lua
+++
@@ -1,5 +0,0 @@
-box.cfg {
-  listen = 3301,
-  log_level = 7, -- debug
-  memtx_memory = 128 * 1024 * 1024 -- 128 Mb
-}
---
+++ src/main/tarantool/xtdb/init.lua
@@ -0,0 +1,51 @@
+local xtdb = {}
+
+local db = require('xtdb.db')
+local error = require('xtdb.error')
+local log = require('log')
+local migrator = require('xtdb.migrator')
+local response = require('xtdb.response')
+local utils = require('xtdb.utils')
+local validator = require('xtdb.validator')
+
+function xtdb.setup(config)
+  local api = {}
+
+  --
+  -- Bootstrap
+  --
+
+  config = validator.validate(config)
+  db.configure(config).create_db()
+  migrator.migrate(config)
+
+
+
+  --
+  -- Models
+  --
+
+  local tx_log = require('xtdb.model.tx_log').model(config)
+
+
+
+  --
+  -- API methods
+  --
+
+  function api.submit_tx(events)
+    if validator.not_blank_string(events) then
+      local tuple = tx_log.submit_tx(events)
+      log.info("submit_tx: %s", utils.to_json(tuple))
+      return response.ok(tx_log.serialize(tuple))
+    else
+      log.warn("submit_tx: invalid params - %s", events)
+      return response.error(error.BAD_REQUEST)
+    end
+
+  end
+
+  return api
+end
+
+return xtdb
