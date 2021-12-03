2c340bb5a6dab385ba337511f87925ca926d1ca7
Ilshat Sultanov <just.sultanov@gmail.com>
2021-12-03 03:06:17 +0300

    Add `try-catch` helper function

--- src/main/tarantool/xtdb/utils.lua
+++ src/main/tarantool/xtdb/utils.lua
@@ -11,4 +11,11 @@ function utils.to_json(data)
   return json.encode(data)
 end
 
+function utils.try(f, catch_f)
+  local status, exception = pcall(f)
+  if not status and catch_f then
+    catch_f(exception)
+  end
+end
+
 return utils
