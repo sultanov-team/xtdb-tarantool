dfe77d82b44538c5d331eb93ab8cef4abe8a4093
Ilshat Sultanov <just.sultanov@gmail.com>
2021-12-04 01:22:47 +0300

    Add `tx-log` API

--- src/main/clojure/xtdb/tarantool.clj
+++ src/main/clojure/xtdb/tarantool.clj
@@ -2,15 +2,42 @@
   (:require
     [clojure.spec.alpha :as s]
     [clojure.string :as str]
-    [xtdb.system :as system])
+    [juxt.clojars-mirrors.nippy.v3v1v1.taoensso.nippy :as nippy]
+    [xtdb.api :as xt]
+    [xtdb.db :as db]
+    [xtdb.io :as xio]
+    [xtdb.system :as system]
+    [xtdb.tx.event :as xte]
+    [xtdb.tx.subscribe :as tx-sub])
   (:import
+    (clojure.lang
+      PersistentHashSet
+      PersistentList
+      PersistentTreeSet
+      PersistentVector)
     (io.tarantool.driver.api
       TarantoolClient
       TarantoolClientFactory)
     (io.tarantool.driver.api.retry
       TarantoolRequestRetryPolicies$AttemptsBoundRetryPolicyFactory$Builder)
     (io.tarantool.driver.mappers
+      DefaultMessagePackMapper
       DefaultMessagePackMapperFactory)
+    (java.io
+      Closeable)
+    (java.lang
+      AutoCloseable)
+    (java.time
+      Duration
+      Instant)
+    (java.time.temporal
+      ChronoUnit)
+    (java.util
+      ArrayList
+      Date
+      List)
+    (java.util.concurrent
+      CompletableFuture)
     (java.util.function
       Function
       UnaryOperator)))
@@ -20,11 +47,66 @@
 
 
 ;;
-;; Default mappers and builders
+;; Helper functions
+;;
+
+(defn to-inst
+  "Converts microseconds to `java.util.Date`."
+  [^long ts]
+  (-> (Instant/EPOCH)
+      (.plus ts ChronoUnit/MICROS)
+      (Date/from)))
+
+
+(defprotocol ITarantoolCallFnArgument
+  "This protocol is used to simplify manipulations with the arguments of the function defined in the Tarantool instance."
+  :extend-via-metadata true
+  (-prepare-fn-args [argument]))
+
+
+(extend-protocol ITarantoolCallFnArgument
+  PersistentList
+  (-prepare-fn-args [coll] (ArrayList. coll))
+
+  PersistentVector
+  (-prepare-fn-args [coll] (ArrayList. coll))
+
+  PersistentHashSet
+  (-prepare-fn-args [coll] (ArrayList. coll))
+
+  PersistentTreeSet
+  (-prepare-fn-args [coll] (ArrayList. coll))
+
+  Object
+  (-prepare-fn-args [object] (-prepare-fn-args [object]))
+
+  nil
+  (-prepare-fn-args [_] (ArrayList.)))
+
+
+(defn prepare-fn-args
+  "Prepares the list of arguments for executing the function defined in the Tarantool instance."
+  (^List [] (-prepare-fn-args nil))
+  (^List [x] (-prepare-fn-args x)))
+
+
+(defn serialize
+  [x]
+  (nippy/freeze-to-string x))
+
+
+(defn deserialize
+  [x]
+  (nippy/thaw-from-string x))
+
+
+
+;;
+;; Default mappers
 ;;
 
 (def ^{:doc "Modification-safe instance of the messagepack mapper.
-  The instance contains converters for simple types and complex types java.util.Map and java.util.List."}
+  The instance contains converters for simple types and complex types `java.util.Map` and `java.util.List`."}
   default-complex-types-mapper
   (.defaultComplexTypesMapper (DefaultMessagePackMapperFactory/getInstance)))
 
@@ -34,6 +116,11 @@
   default-simple-type-mapper
   (.defaultSimpleTypeMapper (DefaultMessagePackMapperFactory/getInstance)))
 
+
+
+;;
+;; Default builders
+;;
 
 (defn default-exception-handler
   "Returns a default exception handler."
@@ -55,6 +142,41 @@
 
 
 
+;;
+;; Tarantool API
+;;
+
+(defn close
+  "Closes the component."
+  [^AutoCloseable x]
+  (.close x))
+
+
+(defn execute
+  "Execute a function defined on the Tarantool instance.
+  Params:
+    * tnt-client - instance of `io.tarantool.driver.api.TarantoolClient`
+    * mapper     - mapper for arguments object-to-MessagePack entity conversion and result values conversion
+    * fn-name    - function name, must not be null or empty
+    * fn-args    - function arguments (optional)"
+  ([^TarantoolClient tnt-client ^DefaultMessagePackMapper mapper ^String fn-name]
+   (execute tnt-client mapper fn-name nil))
+  ([^TarantoolClient tnt-client ^DefaultMessagePackMapper mapper ^String fn-name fn-args]
+   (-> ^CompletableFuture (.call tnt-client fn-name (prepare-fn-args fn-args) mapper)
+       (.get)
+       (first))))
+
+
+(defn get-box-info
+  [^TarantoolClient tnt-client ^DefaultMessagePackMapper mapper]
+  (execute tnt-client mapper "box.info"))
+
+
+
+;;
+;; Tarantool client
+;;
+
 ;; Specs
 
 (s/def ::host ::system/string)
@@ -66,14 +188,14 @@
 (s/def ::request-retry-policy #(instance? UnaryOperator %))
 
 
-;; Tarantool client
+;; Component
 
 (defn ^TarantoolClient ->tnt-client
   "Returns a tarantool client as a component of the xtdb.system."
   {::system/args {:host                 {:doc       "Host"
                                          :spec      ::host
-                                         :required? true
-                                         :default   "localhost"}
+                                         :default   "localhost"
+                                         :required? true}
                   :port                 {:doc       "Port"
                                          :spec      ::port
                                          :default   3301
@@ -84,15 +206,15 @@
                   :password             {:doc       "Password"
                                          :spec      ::password
                                          :required? true}
-                  :retries              {:doc       "Retries"
+                  :retries              {:doc       "The number of retry attempts for each request"
                                          :spec      ::retries
                                          :default   3
                                          :required? true}
-                  :exception-handler    {:doc       "Exception handler"
+                  :exception-handler    {:doc       "Function checking whether the given exception may be retried"
                                          :spec      ::exception-handler
                                          :default   (default-exception-handler)
                                          :required? true}
-                  :request-retry-policy {:doc       "Request retry policy"
+                  :request-retry-policy {:doc       "Retry policy that performs unbounded number of attempts. If the exception check passes, the policy returns true"
                                          :spec      ::request-retry-policy
                                          :default   (default-request-retry-policy {:delay 300})
                                          :required? true}}}
@@ -104,3 +226,91 @@
     (-> (TarantoolClientFactory/configureClient tnt-client)
         (.withRetryingByNumberOfAttempts retries exception-handler request-retry-policy)
         (.build))))
+
+
+
+;;
+;; API tx-log
+;;
+
+(defn submit-tx
+  "Submits a `tx` to the `tx-log` and returns an instance of `clojure.lang.Delay`."
+  [{:keys [tnt-client mapper]} tx-events]
+  (let [{:strs [status data]} (execute tnt-client mapper "xtdb.tx_log.submit_tx" (serialize tx-events))]
+    (when (= 201 status)
+      (let [{:strs [tx_id tx_time]} data]
+        (delay {::xt/tx-id   tx_id
+                ::xt/tx-time (to-inst tx_time)})))))
+
+
+(defn open-tx-log*
+  "Returns the `tx-log` as a lazy sequence."
+  [{:as tx-log :keys [tnt-client mapper]} after-tx-id]
+  (let [txs (lazy-seq
+              (let [{:strs [status data]} (execute tnt-client mapper "xtdb.tx_log.open_tx_log" after-tx-id)]
+                (when (= 200 status)
+                  (let [txs        (map (fn [{:strs [tx_id tx_time tx_events]}]
+                                          {::xt/tx-id      tx_id
+                                           ::xt/tx-time    (to-inst tx_time)
+                                           ::xte/tx-events (deserialize tx_events)})
+                                        data)
+                        last-tx-id (::xt/tx-id (last txs))]
+                    (cons txs (open-tx-log* tx-log last-tx-id))))))]
+    (mapcat identity txs)))
+
+
+(defn open-tx-log
+  "Returns the `tx-log` as an instance of `xtdb.api.Cursor`."
+  [tx-log after-tx-id]
+  (xio/->cursor (constantly false) (open-tx-log* tx-log after-tx-id)))
+
+
+(defn subscribe
+  "Subscribe to the `tx-log` (via long-polling) using a `poll-wait-duration`."
+  [{:as tx-log :keys [poll-wait-duration]} after-tx-id f]
+  (tx-sub/handle-polling-subscription tx-log after-tx-id {:poll-sleep-duration poll-wait-duration} f))
+
+
+(defn latest-submitted-tx
+  "Returns the latest submitted `tx-id` if the `tx-log` is not empty. Otherwise, returns `nil`."
+  [{:keys [tnt-client mapper]}]
+  (when-let [{:strs [status data]} (execute tnt-client mapper "xtdb.tx_log.latest_submitted_tx")]
+    (when (= 200 status)
+      (let [{:strs [tx_id]} data]
+        {::xt/tx-id tx_id}))))
+
+
+(defrecord TarantoolTxLog
+  [^TarantoolClient tnt-client ^DefaultMessagePackMapper mapper ^Duration poll-wait-duration]
+  db/TxLog
+  (submit-tx [tx-log tx-events] (submit-tx tx-log tx-events))
+  (open-tx-log [tx-log after-tx-id] (open-tx-log tx-log after-tx-id))
+  (subscribe [tx-log after-tx-id f] (subscribe tx-log after-tx-id f))
+  (latest-submitted-tx [tx-log] (latest-submitted-tx tx-log))
+
+  Closeable
+  (close [_] (close tnt-client)))
+
+
+
+;; Specs
+
+(s/def ::mapper #(instance? DefaultMessagePackMapper %))
+(s/def ::duration ::system/duration)
+
+
+
+;; Component
+
+(defn ->tx-log
+  {::system/args {:mapper             {:doc       "Default implementation of MessagePackObjectMapper and MessagePackValueMapper"
+                                       :spec      ::mapper
+                                       :default   default-complex-types-mapper
+                                       :required? true}
+                  :poll-wait-duration {:doc       "How long to wait when polling the Tarantool instance"
+                                       :spec      ::duration
+                                       :default   (Duration/ofSeconds 1)
+                                       :required? true}}
+   ::system/deps {:tnt-client `->tnt-client}}
+  [opts]
+  (map->TarantoolTxLog opts))
--- src/test/clojure/xtdb/tarantool_test.clj
+++ src/test/clojure/xtdb/tarantool_test.clj
@@ -1,13 +1,16 @@
 (ns xtdb.tarantool-test
   (:require
     [clojure.test :refer [deftest testing is]]
+    [xtdb.api :as xt]
     [xtdb.system :as system]
-    [xtdb.tarantool :as sut])
+    [xtdb.tarantool :as sut]
+    [xtdb.tx.event :as xte])
   (:import
     (io.tarantool.driver.core
       RetryingTarantoolTupleClient)
+    (java.util
+      ArrayList)
     (java.util.concurrent
-      CompletableFuture
       ExecutionException)
     (java.util.function
       Function
@@ -18,7 +21,7 @@
 ;; Helper functions
 ;;
 
-(defn prep-module
+(defn prep-tnt-client
   [overrides]
   (try
     (-> {:tnt-client (merge {:xtdb/module 'xtdb.tarantool/->tnt-client} overrides)}
@@ -28,19 +31,12 @@
       (ex-message (ex-cause e)))))
 
 
-(defn start-module
+(defn start-tnt-client
   [overrides]
   (-> {:tnt-client (merge {:xtdb/module 'xtdb.tarantool/->tnt-client} overrides)}
       (system/prep-system)
       (system/start-system)
       :tnt-client))
-
-
-(defn get-box-info
-  [tnt-client]
-  (-> ^CompletableFuture (.call tnt-client "box.info" [] sut/default-complex-types-mapper)
-      (.get)
-      (first)))
 
 
 (defn includes?
@@ -68,85 +64,167 @@
 
 
 
+(deftest to-inst-test
+  (testing "should be returned an instance of `java.util.Date`"
+    (is (= #inst"2021-12-03T01:26:09.506-00:00" (sut/to-inst 1638494769506799)))))
+
+
+
+(deftest prepare-fn-args-test
+  (testing "should be returned an instance of `java.util.ArrayList`"
+    (doseq [x [nil 1 1/2 "string" \c :keyword ::qualified-keyword 'symbol 'qualified-symbol '(1 2 3) [1 2 3] #{1 2 3} (ArrayList. [1 2 3])]]
+      (is (instance? ArrayList (sut/prepare-fn-args x))))))
+
+
+
 (deftest ^:unit ->tnt-client-specs-test
   (testing "should be failed by specs"
 
     (is (includes? #"Arg :username required"
-                   (prep-module {})))
+                   (prep-tnt-client {})))
 
     (is (includes? #"Arg :username = 123[\s\S]+:xtdb\.tarantool\/username"
-                   (prep-module {:username 123})))
+                   (prep-tnt-client {:username 123})))
 
     (is (includes? #"Arg :password required"
-                   (prep-module {:username "root"})))
+                   (prep-tnt-client {:username "root"})))
 
     (is (includes? #"Arg :password = 123[\s\S]+:xtdb\.tarantool\/password"
-                   (prep-module {:username "root"
-                                 :password 123})))
+                   (prep-tnt-client {:username "root"
+                                     :password 123})))
 
     (is (includes? #"Arg :host = 123[\s\S]+:xtdb\.tarantool\/host"
-                   (prep-module {:username "root"
-                                 :password "root"
-                                 :host     123})))
+                   (prep-tnt-client {:username "root"
+                                     :password "root"
+                                     :host     123})))
 
     (is (includes? #"Arg :port = -123[\s\S]+:xtdb\.tarantool\/port"
-                   (prep-module {:username "root"
-                                 :password "root"
-                                 :host     "127.0.0.1"
-                                 :port     -123})))
+                   (prep-tnt-client {:username "root"
+                                     :password "root"
+                                     :host     "127.0.0.1"
+                                     :port     -123})))
 
     (is (includes? #"Arg :exception-handler = 123[\s\S]+:xtdb\.tarantool\/exception-handler"
-                   (prep-module {:username          "root"
-                                 :password          "root"
-                                 :exception-handler 123})))
+                   (prep-tnt-client {:username          "root"
+                                     :password          "root"
+                                     :exception-handler 123})))
 
     (is (includes? #"Arg :request-retry-policy = 123[\s\S]+:xtdb\.tarantool\/request-retry-policy"
-                   (prep-module {:username             "root"
-                                 :password             "root"
-                                 :exception-handler    (sut/default-exception-handler)
-                                 :request-retry-policy 123})))
+                   (prep-tnt-client {:username             "root"
+                                     :password             "root"
+                                     :exception-handler    (sut/default-exception-handler)
+                                     :request-retry-policy 123})))
 
     (is (includes? #"Arg :retries = -123[\s\S]+:xtdb\.tarantool\/retries"
-                   (prep-module {:username             "root"
-                                 :password             "root"
-                                 :exception            (sut/default-exception-handler)
-                                 :request-retry-policy (sut/default-request-retry-policy {:delay 500})
-                                 :retries              -123}))))
+                   (prep-tnt-client {:username             "root"
+                                     :password             "root"
+                                     :exception            (sut/default-exception-handler)
+                                     :request-retry-policy (sut/default-request-retry-policy {:delay 500})
+                                     :retries              -123}))))
 
 
   (testing "should be passed by specs"
-    (is (nil? (prep-module {:username "root" :password "root"})))
+    (is (nil? (prep-tnt-client {:username "root" :password "root"})))
 
-    (is (nil? (prep-module {:username "root" :password "root" :exception-handler (sut/default-exception-handler)})))
+    (is (nil? (prep-tnt-client {:username "root" :password "root" :exception-handler (sut/default-exception-handler)})))
 
-    (is (nil? (prep-module {:username             "root"
-                            :password             "root"
-                            :exception-handler    (sut/default-exception-handler)
-                            :request-retry-policy (sut/default-request-retry-policy {:delay 500})})))
+    (is (nil? (prep-tnt-client {:username             "root"
+                                :password             "root"
+                                :exception-handler    (sut/default-exception-handler)
+                                :request-retry-policy (sut/default-request-retry-policy {:delay 500})})))
 
-    (is (nil? (prep-module {:username             "root"
-                            :password             "root"
-                            :exception-handler    (sut/default-exception-handler)
-                            :request-retry-policy (sut/default-request-retry-policy {:delay 500})
-                            :retries              3})))))
+    (is (nil? (prep-tnt-client {:username             "root"
+                                :password             "root"
+                                :exception-handler    (sut/default-exception-handler)
+                                :request-retry-policy (sut/default-request-retry-policy {:delay 500})
+                                :retries              3})))))
 
 
 
 (deftest ^:integration ->tnt-client-test
   (testing "should be throw an exception with a bad connection options"
-    (let [tnt-client (start-module {:host     "localhost"
-                                    :port     3302
-                                    :username "root"
-                                    :password "root"})]
+    (let [tnt-client (start-tnt-client {:host     "localhost"
+                                        :port     3302
+                                        :username "root"
+                                        :password "root"})]
       (is (instance? RetryingTarantoolTupleClient tnt-client))
       (is (thrown-with-msg? ExecutionException #"The client is not connected to Tarantool server"
-            (get-box-info tnt-client)))))
+            (sut/get-box-info tnt-client sut/default-complex-types-mapper)))))
 
 
-  (testing "should be returned a successful response from tarantool"
-    (let [tnt-client (start-module {:username "root"
-                                    :password "root"})
-          box-info   (get-box-info tnt-client)]
+  (testing "should be returned a successful response from the tarantool"
+    (let [tnt-client (start-tnt-client {:username "root", :password "root"})
+          box-info   (sut/get-box-info tnt-client sut/default-complex-types-mapper)]
       (is (instance? RetryingTarantoolTupleClient tnt-client))
       (is (= "0.0.0.0:3301" (get box-info "listen")))
-      (is (includes? #"2.8.2" (get box-info "version"))))))
+      (is (includes? #"2.8.2" (get box-info "version")))
+      (sut/close tnt-client))))
+
+
+
+(deftest ^:integration ->tx-log-test
+  (let [tnt-client (start-tnt-client {:username "root", :password "root"})
+        mapper     sut/default-complex-types-mapper
+        tx-log     {:tnt-client tnt-client, :mapper mapper}
+        truncate   #(sut/execute tnt-client mapper "xtdb.db.truncate")]
+    (testing "should be returned the same tx-id"
+      (truncate)
+      (is (nil? (sut/latest-submitted-tx tx-log)))
+      (let [tx        @(sut/submit-tx tx-log [[::xt/put {:xt/id "hi2u", :user/name "zig"}]])
+            latest-tx (sut/latest-submitted-tx tx-log)
+            txs       (sut/open-tx-log* tx-log nil)]
+        (is (every? seq [tx latest-tx txs]))
+        (is (= #{::xt/tx-id} (->> latest-tx (keys) (set))))
+        (is (= #{::xt/tx-id ::xt/tx-time} (->> tx (keys) (set))))
+        (is (= #{::xt/tx-id ::xt/tx-time ::xte/tx-events} (->> txs (map keys) (flatten) (set))))
+        (is (= (::xt/tx-id tx) (::xt/tx-id latest-tx) (::xt/tx-id (last txs)))))
+      (truncate)
+      (Thread/sleep 5000)
+      (sut/close tnt-client))))
+
+
+
+(comment
+
+  (def node
+    (xt/start-node
+      {::tnt-client      {:xtdb/module `sut/->tnt-client
+                          :username    "root"
+                          :password    "root"}
+
+       :xtdb/index-store {:xtdb/module 'xtdb.kv.index-store/->kv-index-store}
+       :xtdb/tx-log      {:xtdb/module `sut/->tx-log
+                          :tnt-client  ::tnt-client}}))
+
+  (sut/close node)
+
+
+  (xt/submit-tx node [[::xt/put {:xt/id "hi2u", :user/name "zig"}]])
+  ;; => #:xtdb.api{:tx-id 4, :tx-time #inst"2021-12-03T22:00:34.561-00:00"}
+
+
+  (xt/q (xt/db node) '{:find  [e]
+                       :where [[e :user/name "zig"]]})
+  ;; => #{["hi2u"]}
+
+
+  (xt/q (xt/db node)
+        '{:find  [(pull ?e [*])]
+          :where [[?e :xt/id "hi2u"]]})
+  ;; => #{[{:user/name "zig", :xt/id "hi2u"}]}
+
+
+
+  (def history (xt/entity-history (xt/db node) "hi2u" :desc {:with-docs? true}))
+  ;;=> [#:xtdb.api{:tx-time #inst"2021-12-03T22:00:34.561-00:00",
+  ;;               :tx-id 4,
+  ;;               :valid-time #inst"2021-12-03T22:00:34.561-00:00",
+  ;;               :content-hash #xtdb/id"32f90c7020097d1232d14a9af395fc6aabd02ad7",
+  ;;               :doc {:user/name "zig", :xt/id "hi2u"}}]
+
+
+  (->> (map ::xt/doc history)
+       (filter #(= (get % :user/name) "zig")))
+  ;; => ({:user/name "zig", :xt/id "hi2u"})
+
+  )
